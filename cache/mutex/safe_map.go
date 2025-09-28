db714b1bea7f1a6f7ebc39c4a002b4313e740f61
liqinzuo <liqinzuo@bytedance.com>
2025-09-28 16:46:21 +0800

    mod:add safe map double check

---
+++ cache/mutex/safe_map.go
@@ -0,0 +1,44 @@
+package mutex
+
+import "sync"
+
+type SafeMap[K comparable, V any] struct {
+	data  map[K]V
+	mutex sync.RWMutex
+}
+
+func (s *SafeMap[K, V]) Put(key K, value V) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.data[key] = value
+}
+
+func (s *SafeMap[K, V]) Get(key K) (any, bool) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	res, ok := s.data[key]
+	return res, ok
+}
+
+// double-check写法
+func (s *SafeMap[K, V]) LoadOrStore(key K, newValue V) (val V, loaded bool) {
+	s.mutex.RLock()
+	res, ok := s.data[key]
+	s.mutex.RUnlock()
+	if ok {
+		return res, true
+	}
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	// double-check begin
+	res, ok = s.data[key]
+	if ok {
+		return res, true
+	}
+	// double-check end
+
+	s.data[key] = newValue
+	return newValue, false
+}
