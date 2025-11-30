f670c8275515abe3b983035582b7660735af4cc7
liqinzuo <liqinzuo@bytedance.com>
2025-11-30 10:31:49 +0800

    mod:add sync.pool

---
+++ cache/mutex/pool_test.go
@@ -0,0 +1,22 @@
+package mutex
+
+import (
+	"sync"
+	"testing"
+)
+
+// 资源位被复用
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			t.Log("创建资源了。。。。")
+			return "hello"
+		},
+	}
+	str := pool.Get().(string)
+	t.Log(str)
+	pool.Put(str)
+	str = pool.Get().(string)
+	t.Log(str)
+	pool.Put(str)
+}
